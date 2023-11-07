25def1cd83d48603b47e07088783d22f7b7b17db
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-11-07 09:00:34 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 23
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1
+// false => 0
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+//  "" => false
+//  "Mustafa" => true
+
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
