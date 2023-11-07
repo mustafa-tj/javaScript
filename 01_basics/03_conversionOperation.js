4c03ce037390ff7c00aa0054587eb60d3ec96efa
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-11-07 12:21:09 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = 23
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0 => false
@@ -24,5 +24,42 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 45
 let stringNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof someNumber);
+// console.log(someNumber);
+// console.log(typeof someNumber);
+
+// ----------OPERATIONS------------ 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(7+7);
+// console.log(7-7);
+// console.log(7*7);
+// console.log(8/4);
+// console.log(7**7);
+// console.log(7%2);  // modulo
+
+let str = "Hello"
+let str2 = " Mustafa"
+let str3 = str + str2
+
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+let gameCounter = 100
+gameCounter++;
+// ++gameCounter;
+
+console.log(gameCounter);
