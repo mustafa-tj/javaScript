a10373293f8f72247c8d3b16405dc6124bf24d78
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-11-07 08:52:26 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(9 + 4) // we are using nodejs, not browser
+
+// console.log(5 
+//     +
+//     2);  // code readability should high
+
+
+let name = "Mustafa"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//  bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof "Musta"); 
+console.log(typeof age );
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined 
+console.log(typeof isLoggedIn);
