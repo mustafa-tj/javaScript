c580ae95bedc19d4a85848c749d4d1435407211e
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-11-08 12:20:47 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+let name = "Mustafa"
+let repoCount = 19
+
+// console.log(name + repoCount + "Value"); // Not recommended
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Script-mania")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('m'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(2,5)
+// console.log(anotherString);
+
+const stringWithSpace = "   Mustafa   "
+// console.log(stringWithSpace);
+// console.log(stringWithSpace.trim());
+
+const url = "https://google.com/mustafa%20tj"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('mustafa'));
+
+console.log(url.split('/'));
