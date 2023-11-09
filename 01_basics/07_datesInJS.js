bda072fd0071a1effd23a3834839e3cc3ca81d2f
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-11-09 11:10:48 +0000

    Date in JS

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0 ,23)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDateWithTime = new Date(2023, 0 ,23,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000)); // Date in seconds(Interview question)
+
+// have a look at methods of date.
