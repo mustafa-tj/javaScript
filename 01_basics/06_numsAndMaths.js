1c16f9301a981a745167361674f28511ba52b68f
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-11-09 06:41:47 +0000

    Math And Number

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,33 @@
+// ---------------------Nums--------------------------
+const score = 30
+
+const balance = new Number(500)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 125.5253
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -------------------------Maths------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-52));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(2,4,8,6,1));
+// console.log(Math.max(2,4,8,6,1));
+
+// console.log(Math.random() * 10);
+// console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.round(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
